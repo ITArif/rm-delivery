b4f6b396581798629b4cc458ecb3e0bb54d43770
santu-sarker <mdsantusarker@gmail.com>
2023-02-08 15:56:43 +0600

    agent setup

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 // Route::get('/', function () {
 //     return view('welcome');
@@ -22,13 +22,12 @@ use Illuminate\Support\Facades\Route;
 // Auth::routes();
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::get('/login',[LoginController::class,'loginForm'])->name('loginForm');
-Route::post('loginCheck', [LoginController::class,'loginCheck'])->name('loginCheck');
-Route::post('logout', [LoginController::class,'logout'])->name('logout');
+Route::get('/login', [LoginController::class, 'loginForm'])->name('loginForm');
+Route::post('loginCheck', [LoginController::class, 'loginCheck'])->name('loginCheck');
+Route::post('logout', [LoginController::class, 'logout'])->name('logout');
 Route::get('/registration-form', [HomeController::class, 'registerForm'])->name('registerForm');
 Route::post('/register', [HomeController::class, 'registered'])->name('registered');
-
 Route::middleware('admin')->group(function () {
-  Route::get('/admin-dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
-  Route::get('/all-client', [DashboardController::class, 'allClient'])->name('allClient');
+    Route::get('/admin-dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
+    Route::get('/all-client', [DashboardController::class, 'allClient'])->name('allClient');
 });
